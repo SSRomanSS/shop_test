f4e80b19091ea529842d5032a955a5a2d315b452
Roman Shkapiak <r.shkapiak@gmail.com>
2020-05-20 17:13:35 +0300

    Rewrite queries in statistic_view

--- shop_app/views.py
+++ shop_app/views.py
@@ -5,9 +5,8 @@ from django.shortcuts import render
 from django.shortcuts import redirect
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth import login
-from django.db.models import CharField
+from django.db.models import CharField, F, Avg, Max, Sum, Q, Count
 from django.db.models.functions import Length
-from django.db.models import F
 
 from .forms import OrderForm
 from .models import Order
@@ -53,13 +52,17 @@ def statistics_view(request):
     data = Order.objects
     data_dict = {}
     data_dict['users products'] = data.filter(user=user).count()
-    data_dict['all products'] = data.all().count()
-    data_dict['users max price'] = max([query.price for query in data.filter(user=user)])
-    data_dict['avg all prices'] = round(mean([query.price for query in data.all()]))
+    data_dict['all products'] = data.count()
+    # data_dict['users max price'] = max([query.price for query in data.filter(user=user)])
+    # data_dict['user max price'] = data.filter(user=user).aggregate(Max('price')).get('price__max')
+    data_dict['user max price'] = data.filter(user=user).aggregate(max_price=Max('price')).get('max_price')
+    # data_dict['avg all prices'] = round(mean([query.price for query in data.all()]))
+    data_dict['avg all prices'] = round(data.aggregate(Avg('price')).get('price__avg'))
     CharField.register_lookup(Length)  # register lookup
-    data_dict['sum users prices'] = sum([query.price for query in data.filter(user=user)])
+    # data_dict['sum users prices'] = sum([query.price for query in data.filter(user=user)])
+    data_dict['sum users prices'] = data.filter(user=user).aggregate(Sum('price')).get('price__sum')
     data_dict['complex query'] = sum([data.filter(price__gt=50).count(),
-                                      data.filter(user=user).filter(name__length__gt=3).count()])
+                                      data.filter(user=user, name__length__gt=3).count()])
     if request.method == "POST":
         Order.objects.all().update(price=F('price') + 1)
         return redirect('statistics')
